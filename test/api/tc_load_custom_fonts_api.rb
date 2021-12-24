719ad89513d7b51d4285af5ffa3a4590c995fa9e
Alexander Ermakov <ermakoff.alexander@gmail.com>
2021-12-24 16:19:30 +0300

    Fixed test

--- test/api/tc_load_custom_fonts_api.rb
+++ test/api/tc_load_custom_fonts_api.rb
@@ -59,3 +59,4 @@ module AsposeImagingCloudTests
 		get_request_tester('LoadCustomFontsTest', "Input image: #{name}; Output format: #{format}", name, request_invoker, properties_tester, folder, storage)
 	end
   end 
+end 
